16f799633a40bcf831a4ac4577ba92d66e4bc0c1
asmathinfo <asmath.info@gmail.com>
2021-09-13 00:50:44 +0530

    Random image upload

--- blogz/home/models.py
+++ blogz/home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from froala_editor.fields import FroalaField
+from .helpers import *
 
 
 
@@ -14,6 +15,13 @@ class BlogModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     upload_to = models.DateTimeField(auto_now=True)
     
+
+    def __str__(self):
+        return self.title
+
+    def save(self , *args, **kwargs): 
+        self.slug = generate_slug(self.title)
+        super(BlogModel, self).save(*args, **kwargs)
 
 
 
