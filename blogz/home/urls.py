6d0b8d09131db1dccfc2be0155260c73592170c5
asmathinfo <asmath.info@gmail.com>
2021-09-13 09:52:51 +0530

    add blog page

--- blogz/home/urls.py
+++ blogz/home/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
         path('' , home , name="base"),
         path('login/' , login_view , name="login_view"),
         path('register/' , register_view , name="register_view"),
+        path('add_blog/' , add_blog, name="add_blog")
 
 
 ]
--- blogz/home/views.py
+++ blogz/home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .form import *
+
+
 
 def home(request):
    
@@ -13,6 +16,13 @@ def home(request):
 def login_view(request):
 
     return render(request , 'login.html')
+
+
+def add_blog(request):
+    context = {'form' : BlogForm}
+
+    return render(request , 'add_blog.html')
+
 
 
 def register_view(request):
